0f7125d058a5e721547a45d573e98177dc0efafb
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-01-27 21:04:39 +0530

    Time: 1205 ms (77.05%), Space: 33.3 MB (98.99%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -1,48 +1,17 @@
-'''https://www.youtube.com/watch?v=wSgrc98d2lI'''
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        
 class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insert_bits(self, num):
-        bit_num = bin(num)[2:].zfill(32)
-        node = self.root
-        for bit in bit_num:
-            if bit not in node.children:
-                node.children[bit] = TrieNode()
-            node = node.children[bit]
-    
-    
-    def find_max_xor(self, num):
-        bit_num = bin(num)[2:].zfill(32)
-        node = self.root
-        max_xor = ''
-        for bit in bit_num:
-            if bit == '0':
-                oppo_bit = '1'
-            elif bit == '1':
-                oppo_bit = '0'
-            
-            if oppo_bit in node.children:
-                max_xor += oppo_bit
-                node = node.children[oppo_bit]
-            else:
-                max_xor += bit
-                node = node.children[bit]
-        
-        return int(max_xor, 2) ^ num
-    
-    
     def findMaximumXOR(self, nums: List[int]) -> int:
-        for num in nums:
-            self.insert_bits(num)
-        
-        max_ = 0
-        for num in nums:
-            max_ = max(max_, self.find_max_xor(num))
-            
-        return max_
+        # length of max number in a binary representation
+        L = len(bin(max(nums))) - 2
+        max_xor = 0
+        for i in range(L)[::-1]:
+        # go to the next bit by the left shift
+            max_xor <<= 1
+            # set 1 in the smallest bit
+            curr_xor = max_xor | 1
+            # compute all existing prefixes 
+            # of length (L - i) in binary representation
+            prefixes = {num >> i for num in nums}
+            # Update max_xor, if two of these prefixes could result in curr_xor.
+            # Check if p1^p2 == curr_xor, i.e. p1 == curr_xor^p2
+            max_xor |= any(curr_xor^p in prefixes for p in prefixes)
+        return max_xor
