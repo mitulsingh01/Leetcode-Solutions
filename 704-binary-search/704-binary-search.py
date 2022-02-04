23366492049280809fad233efc7e10465de09565
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-02-04 12:32:11 +0530

    Time: 456 ms (8.83%), Space: 15.6 MB (65.42%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,14 +1,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
-        r = len(nums) - 1
-        while l <= r:
-            mid = l + (r-l) // 2
-            if nums[mid] == target:
+        r = len(nums)-1
+        index = self.binarySearch(nums, l, r, target)
+        return index
+    
+    def binarySearch(self, arr, l, r, x):
+  
+        # Check base case
+        if r >= l:
+
+            mid = l + (r - l) // 2
+
+            # If element is present at the middle itself
+            if arr[mid] == x:
                 return mid
-            elif nums[mid] < target:
-                l = mid+1
-            elif nums[mid] > target:
-                r = mid-1
-                
-        return -1
+
+            # If element is smaller than mid, then it
+            # can only be present in left subarray
+            elif arr[mid] > x:
+                return self.binarySearch(arr, l, mid-1, x)
+
+            # Else the element can only be present
+            # in right subarray
+            else:
+                return self.binarySearch(arr, mid + 1, r, x)
+
+        else:
+            # Element is not present in the array
+            return -1
