169640b90aa0df0e50285973f4cd6f10cdd9b30b
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-02-18 21:30:38 +0530

    Time: 52 ms (76.87%), Space: 14.5 MB (77.52%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        char = {}
+        char2 = {}
+        for i in s:
+            if i in char:
+                char[i] += 1
+            else:
+                char[i] = 1
+        for i in t:
+            if i in char2:
+                char2[i] += 1
+            else:
+                char2[i] = 1
+        return char == char2
