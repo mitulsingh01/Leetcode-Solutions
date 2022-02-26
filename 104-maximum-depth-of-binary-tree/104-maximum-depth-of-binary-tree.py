a09104e6d9ff41d7ba959e9e3d442b1aaf7be3a2
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-02-26 21:47:00 +0530

    Time: 82 ms (10.28%), Space: 15.3 MB (93.63%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -12,7 +12,7 @@ class Solution:
         """return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))"""
         
         #iterative dfs
-        stack = [[root, 1]]
+        """stack = [[root, 1]]
         res = 0
         
         while stack:
@@ -21,4 +21,17 @@ class Solution:
                 res = max(res, depth)
                 stack.append([node.left, depth+1])
                 stack.append([node.right, depth+1])
-        return res
+        return res"""
+        
+        #bfs
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
