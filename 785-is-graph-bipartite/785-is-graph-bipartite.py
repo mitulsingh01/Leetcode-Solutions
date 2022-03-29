fe1cca1e0a503360d12dbf530a74889df249e698
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-03-29 20:54:30 +0530

    Time: 383 ms (9.63%), Space: 14.4 MB (51.80%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -18,3 +18,25 @@ class Solution:
                             queue.append((neighbor, 1 if level == 0 else 0))
                     
         return True
+    
+    
+    #DFS
+    """
+    def dfs_color(i):
+		for nb in graph[i]:
+			if nb in color:
+				if color[nb] == color[i]:
+					return False
+			else:
+				color[nb] = 1 - color[i]
+				if not dfs_color(nb):
+					return False
+		return True
+	color = {}
+	for i in range(len(graph)):
+		if i not in color:
+			color[i] = 0
+			if not dfs_color(i):
+				return False
+	return True
+    """
