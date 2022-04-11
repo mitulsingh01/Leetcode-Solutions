4100988f050c28e2d4068788709412598fba4fbe
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-04-11 17:59:11 +0530

    Time: 130 ms (21.66%), Space: 15.8 MB (30.22%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,6 +1,21 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        count = {}
+        if  not nums:
+            return 0
+
+        freq = [0] * (max(nums)+1)
+        for n in nums:
+            freq[n] += n
+
+        dp = [0] * len(freq)
+        dp[1] = freq[1]
+        for i in range(2, len(freq)):
+            dp[i] = max(freq[i] + dp[i-2], dp[i-1])
+
+        return dp[len(freq)-1]
+        
+        
+        """count = {}
         for i in range(len(nums)):
             if nums[i] in count:
                 count[nums[i]] += 1
@@ -20,4 +35,6 @@ class Solution:
                 temp = earn2
                 earn2 = earn2 + curEarn
                 earn1 = temp
-        return earn2
+        return earn2"""
+        
+        
