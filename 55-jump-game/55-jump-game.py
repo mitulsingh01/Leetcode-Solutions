8a9a8bc63416863fed8587335263162d73126563
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-04-12 21:17:53 +0530

    Time: 456 ms (99.74%), Space: 15.1 MB (84.12%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,6 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        length = len(nums)
+        goal = len(nums)-1
+        
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] + i >= goal:
+                goal = i
+        
+        return goal == 0
+        
+        
+        """length = len(nums)
         dp = [0] * length
         
         dp[0] = nums[0]
@@ -15,4 +24,4 @@ class Solution:
             if dp[i] >= length - 1:
                 return True
         
-        return dp[length - 2] >= length - 1
+        return dp[length - 2] >= length - 1"""
