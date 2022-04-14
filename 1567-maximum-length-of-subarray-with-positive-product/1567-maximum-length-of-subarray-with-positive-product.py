bb092f8b346053cb05480ce4a4c92105fa1bab5d
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-04-14 22:06:28 +0530

    Time: 700 ms (77.76%), Space: 28.5 MB (36.68%) - LeetHub

---
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getMaxLen(self, nums: List[int]) -> int:
+        n = len(nums)
+        pos = [0] * n # (1)
+        neg = [0] * n # (2)
+        
+        pos[0] = 1 if nums[0] > 0 else 0
+        neg[0] = 1 if nums[0] < 0 else 0
+        
+        for i in range(1, n):
+            if nums[i] < 0:
+                pos[i] = 1 + neg[i - 1] if neg[i - 1] else 0
+                neg[i] = 1 + pos[i - 1] if pos[i - 1] else 1
+            elif nums[i] > 0:
+                pos[i] = 1 + pos[i - 1] if pos[i - 1] else 1
+                neg[i] = 1 + neg[i - 1] if neg[i - 1] else 0
+                    
+        return max(pos)
