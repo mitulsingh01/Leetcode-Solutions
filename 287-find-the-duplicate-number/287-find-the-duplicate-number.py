a574446d16a18a9cebd7aadd5a0b8ab6cd2df2eb
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-07-07 14:02:38 +0530

    Time: 902 ms (55.95%), Space: 28 MB (31.23%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
+        """nums.sort()
         l = 0
         r = len(nums) - 1
         while l <= r:
@@ -14,5 +14,23 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid - 1
-        return l
+        return l"""
+        
+        slow = 0
+        fast = 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:  # Meeting Point
+                break
+
+        left = 0  # Pointer to detect the repeating element
+
+        while True:
+            slow = nums[slow]
+            left = nums[left]
+            if slow == left:
+                return slow
                 
