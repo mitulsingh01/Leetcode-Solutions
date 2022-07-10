274a8d7990113abaa5a34d23c27319e6589600fc
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-07-11 04:40:48 +0530

    Time: 98 ms (40.85%), Space: 13.9 MB (75.92%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
+        """cost.append(0)
         def recursion(n):
             if dp[n] != 0:
                 return dp[n]
@@ -14,4 +14,12 @@ class Solution:
             dp[n] = cost[n] + min(jump, doubleJump)
             return dp[n]
         dp = [0]*(len(cost) + 1)
-        return recursion(len(cost)-1)
+        return recursion(len(cost)-1)"""
+        
+        cost.append(0)
+        dp = [0]*(len(cost) + 1)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        return dp[len(cost)-1]
