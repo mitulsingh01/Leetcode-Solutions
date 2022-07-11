4b6af2614f5ec917bc587ddd08947df1d9c57c84
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-07-11 16:39:53 +0530

    Time: 56 ms (30.56%), Space: 13.8 MB (73.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,6 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def recursion(row, col):
+        """def recursion(row, col):
             if col == 0 and row == 0:
                 return 1
             if row < 0 or col < 0:
@@ -15,4 +15,16 @@ class Solution:
             return dp[row][col]
         
         dp = [[-1]*n for _ in range(m)]
-        return recursion(m-1, n-1)
+        return recursion(m-1, n-1)"""
+        
+        dp = [[-1]*n for _ in range(m)]
+        
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
