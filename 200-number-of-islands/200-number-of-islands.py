0e4e00c70f546bb18d60b789f2653b307878b196
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-07-27 15:23:25 +0530

    Time: 555 ms (31.96%), Space: 16.4 MB (63.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,6 +3,28 @@ class Solution:
         if not grid:
             return 0
         
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+        
+        
+        
+        """if not grid:
+            return 0
+        
         rows, cols = len(grid), len(grid[0])
         visit = set()
         islands = 0
@@ -26,4 +48,4 @@ class Solution:
                 if grid[r][c] == "1" and (r, c) not in visit:
                     bfs(r, c)
                     islands += 1
-        return islands
+        return islands"""
