7cf9289fcfa6bea21105f42891ef1aa3cc851f4f
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-08-19 14:48:03 +0530

    Time: 81 ms (5.11%), Space: 14.1 MB (97.83%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,21 +6,26 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        queue = [(root, 0)]
-        mapp = collections.defaultdict(list)
-        
-        while queue:
-            nxt = []
-            for node, vertical in queue:
-                mapp[vertical].append(node.val)
-                if node.left:
-                    nxt.append((node.left, vertical - 1))
-                if node.right:
-                    nxt.append((node.right, vertical + 1))
-                #sort the nxt according to vertical
-                nxt.sort(key = lambda x: (x[1], x[0].val))
-            queue = nxt
-        for i in sorted(mapp):
-            res.append(mapp[i])
-        return res
+        m = dict()
+        #create a map of horizontal distance m[hd] = {level:[root.val]}
+        def get_vertical_order(root, hd, level):
+            if not root:
+                return
+            if m.get(hd):
+                if m[hd].get(level):
+                    m[hd][level].append(root.val)
+                else:
+                    m[hd][level] = [root.val]
+            else:
+                m[hd] = {level:[root.val]}
+            get_vertical_order(root.left, hd - 1, level+1)
+            get_vertical_order(root.right, hd + 1, level+1)
+        get_vertical_order(root, 0, 1)
+        ans = []
+        for keys in sorted(m.keys()):
+            temp = []
+            for levels in sorted(m[keys]):
+                temp+=sorted(m[keys][levels])
+            ans.append(temp)
+        return ans
+                
