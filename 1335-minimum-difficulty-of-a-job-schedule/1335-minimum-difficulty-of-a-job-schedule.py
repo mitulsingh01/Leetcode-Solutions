57f680d9aaa74c4b1497b77248d1c229b8248d1e
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-08-22 12:13:52 +0530

    Time: 1181 ms (69.49%), Space: 14 MB (81.68%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,14 +1,40 @@
 class Solution:
-    def minDifficulty(self, A: List[int], d: int) -> int:
-        n = len(A)
-        if n < d: return -1
-        @lru_cache(None)
-        def dfs(i, d):
-            if d == 1:
-                return max(A[i:])
-            res, maxd = float('inf'), 0
-            for j in range(i, n - d + 1):
-                maxd = max(maxd, A[j])
-                res = min(res, maxd + dfs(j + 1, d - 1))
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+        if n < d:
+            return -1
+        dp = [[0] * n] + [[float('inf')] * n for _ in range(d - 1)]
+        dp[0][0] = jobDifficulty[0]
+        for i in range(1, n):
+            dp[0][i] = max(jobDifficulty[i], dp[0][i - 1])
+        for i in range(1, d):
+            for j in range(i, n):
+                max_r = jobDifficulty[j]
+                for r in range(j, i - 1, -1):
+                    max_r = max(max_r, jobDifficulty[r])
+                    dp[i][j] = min(dp[i][j], max_r + dp[i - 1][r - 1])
+        return dp[-1][-1]
+        
+        
+        
+        """n = len(jobDifficulty)
+        if n < d:
+            return -1
+        dp = [[None] * n for _ in range(d + 1)]
+        def dfs(d, i):
+            if i == n and d == 0:
+                return 0
+            if i == n or d == 0:
+                return -1
+            if dp[d][i]:
+                return dp[d][i]
+            max_cur = jobDifficulty[i]
+            res = float('inf')
+            for j in range(i, n):
+                max_cur = max(max_cur, jobDifficulty[j])
+                val = dfs(d - 1, j + 1)
+                if val != -1:
+                    res = min(val + max_cur, res)
+            dp[d][i] = res
             return res
-        return dfs(0, d)
+        return dfs(d, 0)"""
