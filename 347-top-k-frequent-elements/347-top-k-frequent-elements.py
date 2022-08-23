9bf6cdab5ca2944ec9e19d8ad0164fa519038e2f
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-08-23 20:17:35 +0530

    Time: 154 ms (59.56%), Space: 19.8 MB (6.30%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,13 +1,37 @@
 import queue
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if k == len(nums):
+        #Bucket Sort -> TC: O(n)
+        count = {}
+        freq = [[] for _ in range(len(nums)+1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n,c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Heap -> TC: O(klogn)
+        """if k == len(nums):
             return nums
         mp = {}
         for ele in nums:
             if ele not in mp:
                 mp[ele] = 0
             mp[ele] += 1
+            
         heap = queue.PriorityQueue()
         for x in mp:
             heap.put([-mp[x], -x])
@@ -15,5 +39,5 @@ class Solution:
         for i in range(k):
             ans[i] = -heap.get()[1]
         ans.sort()
-        return ans
+        return ans"""
         
