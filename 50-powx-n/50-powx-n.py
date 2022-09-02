46d06f0cf8edd5bef856c4d33dbdcc1252015340
Mitul Singh <56824538+mitulsingh01@users.noreply.github.com>
2022-09-03 02:37:56 +0530

    Time: 39 ms (76.40%), Space: 14 MB (19.74%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,6 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        ans = 1.0
+        if abs(x) < 1e-40: return 0 
+        if n == 0: return 1
+        if n < 0: return self.myPow(1/x, -n)
+        lower = self.myPow(x, n//2)
+        if n % 2 == 0: return lower*lower
+        if n % 2 == 1: return lower*lower*x
+        
+        
+        """ans = 1.0
         nn = n
         if nn < 0:
             nn *= -1
@@ -14,4 +22,4 @@ class Solution:
                 nn /= 2
         if n < 0:
             ans = 1.0/ans
-        return ans
+        return ans"""
